2700e06e8ba2cc2962333c78e8b30cfbf64e3671
hudsonst <nadinegraham@gmail.com>
2020-02-24 19:38:44 -0500

    Updated for deploy

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    API_ENDPOINT: 
-    process.env.REACT_APP_API_BASE_URL ||
-    "http://localhost:8000"
+    API_ENDPOINT: `https://blooming-beyond-09029.herokuapp.com/`
+    //process.env.REACT_APP_API_BASE_URL ||
+    //"http://localhost:8000"
   }
